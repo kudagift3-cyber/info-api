e5e14713950afde53e61db4f432d2e61ecb6fc52
kudagift3-cyber <kudagift3@gmail.com>
2025-09-25 20:07:14 +0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 from app import app
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-    #THIS CODE CREATE BY @JOBAYAR_AHME
-#THIS CODE CREATE BY @JOBAYAR_AHME
-#THIS CODE CREATE BY @JOBAYAR_AHME
-#THIS CODE CREATE BY @JOBAYAR_AHME
-#THIS CODE CREATE BY @JOBAYAR_AHME
-#THIS CODE CREATE BY @JOBAYAR_AHME
+# ✅ No app.run() here for Vercel
+# Vercel will use this app object automatically
